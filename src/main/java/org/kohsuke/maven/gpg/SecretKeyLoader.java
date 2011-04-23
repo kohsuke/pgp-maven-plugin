2871ec2c8d4e70f888e82b78233901360d692cbe
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-04-23 12:32:42 -0700

    implemented another secret key loader

--- src/main/java/org/kohsuke/maven/gpg/SecretKeyLoader.java
+++ src/main/java/org/kohsuke/maven/gpg/SecretKeyLoader.java
@@ -4,10 +4,22 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.bouncycastle.openpgp.PGPSecretKey;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public abstract class SecretKeyLoader {
     public abstract PGPSecretKey load(PgpMojo mojo, String specifier) throws IOException, MojoExecutionException;
+
+    protected final Map<String,String> parseQueryParameters(String specifier) {
+        Map<String,String> opts = new HashMap<String, String>();
+        for (String token : specifier.split("&")) {
+            int idx = token.indexOf('=');
+            if (idx<0)  opts.put(token,"");
+            else        opts.put(token.substring(0,idx),token.substring(idx+1));
+        }
+        return opts;
+    }
 }
---
+++ src/main/java/org/kohsuke/maven/gpg/loaders/KeyRingLoader.java
@@ -0,0 +1,80 @@
+package org.kohsuke.maven.gpg.loaders;
+
+import org.bouncycastle.openpgp.PGPObjectFactory;
+import org.bouncycastle.openpgp.PGPSecretKey;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
+import org.bouncycastle.openpgp.PGPUtil;
+import org.codehaus.plexus.component.annotations.Component;
+import org.kohsuke.maven.gpg.PgpMojo;
+import org.kohsuke.maven.gpg.SecretKeyLoader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Loads a key from a keyring.
+ * 
+ * @author Kohsuke Kawaguchi
+ */
+@Component(role=SecretKeyLoader.class,hint="keyring")
+public class KeyRingLoader extends SecretKeyLoader {
+    public PGPSecretKey load(PgpMojo mojo, String specifier) throws IOException {
+        Map<String,String> opts = parseQueryParameters(specifier);
+
+        File keyFile;
+        if (opts.containsKey("keyring")) {
+            keyFile = new File(opts.get("keyring"));
+        } else {
+            keyFile = new File(new File(System.getProperty("user.home")),".gnupg/secring.gpg");
+        }
+        if (!keyFile.exists())
+            throw new IOException("No such key ring file exists: "+keyFile);
+
+
+        String id = opts.get("id");
+
+        InputStream in = PGPUtil.getDecoderStream(new FileInputStream(keyFile));
+        try {
+            PGPObjectFactory pgpFact = new PGPObjectFactory(in);
+
+            Object              obj;
+            while ((obj = pgpFact.nextObject()) != null)
+            {
+                if (!(obj instanceof PGPSecretKeyRing))
+                    throw new IOException("Expecting a secret key but found "+obj);
+
+                PGPSecretKeyRing key = (PGPSecretKeyRing)obj;
+
+                if (id==null)
+                    return key.getSecretKey();  // pick up the first one if no key ID specifier is given
+
+                Iterator jtr = key.getSecretKeys();
+                while (jtr.hasNext()) {
+                    PGPSecretKey skey = (PGPSecretKey) jtr.next();
+
+                    if (id.equalsIgnoreCase(Long.toHexString(skey.getPublicKey().getKeyID() & 0xFFFFFFFF)))
+                        return skey;
+
+                    for (Iterator ktr=skey.getUserIDs(); ktr.hasNext(); ) {
+                        String s = (String) ktr.next();
+                        if (s.contains(id))
+                            return skey;
+                    }
+
+                }
+            }
+
+            throw new IOException("No key that matches "+id+" was found in "+keyFile);
+        } finally {
+            in.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new KeyRingLoader().load(null, "id=D50582E6");
+    }
+}
